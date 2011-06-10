75173caab0e1b3cee81cd8802d51bc4a5d90f18b
Ameravant <contact@ameravant.com>
2011-06-10 14:13:58 -0700

    add multiple iamges was missing its route

--- routes.rb
+++ routes.rb
@@ -2,7 +2,7 @@ resources :galleries, :has_many => :images
 
 namespace :admin do |admin|
   admin.resources :galleries, :has_many => { :features, :menus }, :collection => { :reorder => :put } do |gallery|
-    gallery.resources :images, :member => { :reorder => :put }, :collection => { :reorder => :put }
+    gallery.resources :images, :member => { :reorder => :put }, :collection => { :reorder => :put, :add_multiple => :get }
     gallery.resources :menus
   end
 end 
