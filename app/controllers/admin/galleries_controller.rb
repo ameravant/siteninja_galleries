6e9cba49648609f4f9dc25ec052b5ddfa51017ff
dmyers722 <dave@LAPTOP-0LBMVEM3>
2025-05-30 16:53:45 -0700

    reverting galleries

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -6,56 +6,13 @@ class Admin::GalleriesController < AdminController
   add_breadcrumb "Galleries", "admin_galleries_path", :only => [:new, :edit, :show]
   
   def index
-    if params[:csv]
-      redirect_to("/admin/galleries.csv")
-    else
-      add_breadcrumb "Galleries"
-      if params[:q].blank?
-        @all_galleries = Gallery.all
-      else
-        @all_galleries = Gallery.find(:all, :conditions => ["title like ?", "%#{params[:q].strip}%"])
-      end
-      @galleries = @all_galleries#.paginate(:page => params[:page], :per_page => 30)
-    end
-
-    # Export CSV
-    respond_to do |wants|
-      require 'fastercsv'
+    add_breadcrumb "Galleries"
+    if params[:q].blank?
       @all_galleries = Gallery.all
-      wants.html
-      wants.csv do
-        @outfile = "galleries_" + Time.now.strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
-        csv_data = FasterCSV.generate do |csv|
-          csv << ["ID", "Title", "Content", "Excerpt", "Date", "Post Type", "Permalink", "Image URL", "Image Title", "Image Caption", "Image Description", "Image Alt Text", "Image Featured", "Attachment URL", "Categories", "Status", "Slug", "Comment Status", "Ping Status", "Post Modified Date"]#, "images_count", "assets_count", "features_count"]
-          @all_galleries.each do |gallery|
-            gallery_body = gallery.rendered_body.blank? ? gallery.body : gallery.rendered_body.gsub('data-src', 'src')
-            i = Image.first(:conditions => {:viewable_id => gallery.id, :viewable_type => "Gallery", :show_as_cover_image => true})
-            i = Image.first(:conditions => {:viewable_id => gallery.id, :viewable_type => "Gallery", :show_in_image_box => true}) if i.blank?
-            i = Image.first(:conditions => {:viewable_id => gallery.id, :viewable_type => "Gallery", :show_in_slideshow => true}) if i.blank?
-            if !i.blank?
-              image_url = i.image(:original)
-              image_title = i.title
-              image_caption = i.caption
-              image_description = i.description
-            else
-              image_url = ""
-              image_title = ""
-              image_caption = ""
-              image_description = ""
-            end
-            images = Image.all(:conditions => {:viewable_id => gallery.id, :viewable_type => "Gallery"}, :order => "show_as_cover_image, position asc")
-            status = gallery.hidden ? "draft" : "publish"
-            
-            csv << [gallery.id, gallery.title, gallery.body, gallery.description, gallery.created_at.strftime("%Y-%m-%d %H:%M:%S"), "gallery", gallery_url(gallery), images.collect{|i| i.image(:original)}.join(','), images.collect{|i| i.title}.join(','), images.collect{|i| i.caption}.join(','), images.collect{|i| i.description}.join(','), images.collect{|i| i.title}.join(','), image_url, "", gallery.gallery_categories.collect{|lc| lc.title}.join(','), status, gallery.permalink, "closed", "closed", gallery.updated_at.strftime("%Y-%m-%d %H:%M:%S")]
-          end
-        end
-        send_data csv_data,
-        :type => 'text/csv; charset=iso-8859-1; header=present',
-        :disposition => "attachment; filename=#{@outfile}"
-        flash[:notice] = "Export complete!"
+    else
+      @all_galleries = Gallery.find(:all, :conditions => ["title like ?", "%#{params[:q].strip}%"])
     end
-
-
+    @galleries = @all_galleries#.paginate(:page => params[:page], :per_page => 30)
   end
   
   def new
